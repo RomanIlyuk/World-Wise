1efe772e899b0dde44584a719f33b961be8dbf1d
RomanIlyuk <ilyukroma228@gmail.com>
2026-02-15 18:35:47 +0200

    Added nested routes for cities and countries

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of all places</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
